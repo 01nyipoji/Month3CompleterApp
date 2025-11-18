a27c9c6ad9dc8166e8df9fc4621e39fe074e2bbb
Nyipoji Kani <nyip2019@gmail.com>
2025-11-18 09:08:51 +0100

    linked javascript to global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,17 +1,44 @@
 const componentElements = document.querySelectorAll('[data-import]');
 
-for (let element of componentElements){
+function renderComponent(elements){
+    for (let element of elements){
     const file = element.getAttribute('data-import');
 
     console.log(file);
     fetch(file)
     .then((res) => {
+        if(!res.ok){
+            throw "Not found";
+        }
         return res.text();
     })
     .then((component) => {
         element.innerHTML = component;
+        loadComponentScripts(element);
     })
     .catch(() => {
         element.innerHTML = `<h4> Component not found</h4>`;
     });
 };
+
+};
+
+renderComponent(componentElements);
+
+function loadComponentScripts(element){
+    const scripts = element.querySelectorAll('script');
+
+    for (let script of scripts){
+        const newScript = document.createElement("script");
+        if(script.src){
+            newScript.src = script.src
+        }
+        if(script.textContent){
+            newScript.textContent = script.textContent;
+        }
+        script.remove();
+
+        document.body.appendChild(newScript);
+    }
+}
+
