ed319cb4796172b71f8f169d64b6c83d68fb1b3b
Nyipoji Kani <nyip2019@gmail.com>
2025-11-18 09:38:58 +0100

    activated signup and login btns

--- templates/login/login.js
+++ templates/login/login.js
@@ -1,1 +1,15 @@
-alert("yo")
+const loginButton = document.getElementById("login-btn");
+const signupButton = document.getElementById("signup-btn");
+
+loginButton.addEventListener("click", ()=>{
+    // loginButton.classList.add("active");
+    // signupButton.classList.remove("active")
+    signupButton.style.color ="red";
+    loginButton.style.color ="blue";
+});
+
+
+signupButton.addEventListener("click", ()=>{
+    signupButton.classList.add("active");
+    loginButton.classList.remove("active")
+});
